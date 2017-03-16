a4ff9ca7b2949fd9f75cfb0a2ff0bc098936792b
laurahuotari <k8237@student.jamk.fi>
2017-03-16 14:57:01 +0200

    alien attack

--- AlienAttackApp/HighScore.xaml.cs
+++ AlienAttackApp/HighScore.xaml.cs
@@ -13,28 +13,28 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace AlienAttackApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class HighScore : Page
     {
-        public MainPage()
+        public HighScore()
         {
             this.InitializeComponent();
         }
 
-        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(GamePage));
-        }
-
-        private void HSButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(HighScore));
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null) return;
+            if (rootFrame.CanGoBack)
+            {
+                rootFrame.GoBack();
+            }
         }
     }
 }
