b721fc5e203b0a618ba2aee3d9ea28f90d2b1e49
Noora Jokinen <K8241@student.jamk.fi>
2017-03-30 13:32:42 +0300

    player-user control

--- AlienAttackApp/GamePage.xaml.cs
+++ AlienAttackApp/GamePage.xaml.cs
@@ -36,5 +36,9 @@ namespace AlienAttackApp
                 rootFrame.GoBack();
             }
         }
+
+
+
+
     }
 }
---
+++ AlienAttackApp/Player.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace AlienAttackApp
+{
+    public sealed partial class Player : UserControl
+    {
+        public Player()
+        {
+            this.InitializeComponent();
+        }
+
+        private readonly double MaxSpeed = 10;
+
+        private double speed;
+
+        public double LocationX { get; set; }
+
+        public void MoveLeft()
+        {
+
+        }
+
+        public void MoveRight()
+        {
+
+        }
+
+        public void Shoot()
+        {
+
+        }
+
+        public void SetLocation()
+        {
+            SetValue(Canvas.LeftProperty, LocationX);
+        }
+
+    }
+}
